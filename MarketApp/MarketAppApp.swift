606fd88d3f98a5066a5f5c1d2a289eeb0e3ff383
Cosmograve <averpsy@gmail.com>
2023-06-27 22:29:53 +0700

    first

---
+++ MarketApp/MarketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketAppApp.swift
+//  MarketApp
+//
+//  Created by Алексей Авер on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MarketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
